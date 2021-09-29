387dcfef6f16527f5e3824273dacad4889f7691d
mark <markpackham1@gmail.com>
2021-09-29 11:35:52 +0100

    add user router, https://youtu.be/SccSCuHhOw0?t=975

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  //   res.download("server.js");
-  //   res.json({ message: "Hi there" });
-  res.render("index", { text: "World" });
-});
+const userRouter = require("./routes/users");
+
+app.use("/users", userRouter);
 
 app.listen(3000);
