01ac5116723d6f569c4712c75af36d48156af2f1
mark <markpackham1@gmail.com>
2021-09-29 14:14:10 +0100

    handle html content, https://youtu.be/SccSCuHhOw0?t=1782

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const app = express();
 
-app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.render("index", { text: "World" });
-});
+app.set("view engine", "ejs");
 
 const userRouter = require("./routes/users");
 
